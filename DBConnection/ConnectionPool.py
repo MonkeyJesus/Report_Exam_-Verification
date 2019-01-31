b3d178bcec54e27d21d8204f1b779a8ef7236fb1
MonkeyJesus <840766053@qq.com>
2019-01-31 18:00:57 +0800

    Update ConnectionPool.py
    update

--- DBConnection/ConnectionPool.py
+++ DBConnection/ConnectionPool.py
@@ -5,11 +5,11 @@ import DBUtils
 from DBUtils.PooledDB import PooledDB
 
 # #account 连接池
-# accountPool = PooledDB(MySQLdb,5,host='114.215.111.29',user='yjw0223',passwd='f9a<)6hh>0pP',db='fzaccount',port=3306, charset="utf8")
+# accountPool = PooledDB(MySQLdb,5,host='',user='',passwd='',db='',port=3306, charset="utf8")
 #
 # #report 连接池
-# reportPool = PooledDB(MySQLdb,5,host='114.215.111.29',user='yjw0223',passwd='f9a<)6hh>0pP',db='fzreport',port=3314, charset="utf8")
+# reportPool = PooledDB(MySQLdb,5,host='',user='',passwd='',db='',port=3314, charset="utf8")
 #
 # #report 连接池
-# examPool = PooledDB(MySQLdb,5,host='114.215.111.29',user='yjw0223',passwd='f9a<)6hh>0pP',db='fzexam',port=3312, charset="utf8")
+# examPool = PooledDB(MySQLdb,5,host='',user='',passwd='',db='',port=3312, charset="utf8")
 
